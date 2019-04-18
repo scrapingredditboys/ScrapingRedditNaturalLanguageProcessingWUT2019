e4f5397df03d1a6fa79e86870a33735efe5408ad
Kamil Czerniak <kamil_czerniak@outlook.com>
2019-04-18 09:31:59 +0200

    Scripts changed to form used to generate data files.

--- data-stats/karma_per_length.py
+++ data-stats/karma_per_length.py
@@ -2,29 +2,45 @@ import pandas as pd
 import pandas.io as io
 from sqlalchemy import create_engine
 import threading
-from multiprocessing.dummy import Pool as ThreadPool 
+from multiprocessing.dummy import Pool as ThreadPool
 import re
+import os.path
+
+from sqlalchemy.util import NoneType
+
 engine = create_engine('postgresql://postgres:password123@localhost:5432/reddit')
 
 
 def subreddit_length(name):
+    # If file exists, then why calculate it again?
+    if os.path.exists("karma_submissions_{0}.csv".format(name)):
+        print("DONE\n")
+        return
     df = pd.DataFrame(columns=["title_length", "selftext_length", "score"])
     df2 = pd.DataFrame(columns=["length", "score"])
 
     submissions = pd.read_sql_query(
-            "SELECT * FROM submissions where subreddit_id=\'{0}\'".format(name),
-            con=engine)
-    for submission_index,submission_row in submissions.iterrows():
-        print("Submission: {0}/{1}".format(submission_index+1, submissions['id'].count()))
-        title_length = len(submission_row['title'])
-        selftext_length = len(submission_row['selftext'])
+        "SELECT * FROM submissions where subreddit_id=\'{0}\'".format(name),
+        con=engine)
+    for submission_index, submission_row in submissions.iterrows():
+        print("Submission: {0}/{1}".format(submission_index + 1, submissions['id'].count()))
+        if type(submission_row['title']) is not NoneType:
+            title_length = len(submission_row['title'])
+        else:
+            title_length = 0
+        if type(submission_row['selftext']) is not NoneType:
+            selftext_length = len(submission_row['selftext'])
+        else:
+            selftext_length = 0
         df.loc[len(df)] = [title_length, selftext_length, submission_row['score']]
 
         # Comments
         comments = pd.read_sql_query(
-            "SELECT * FROM comments where parent_id=\'"+submission_row['id']+"\'",
+            "SELECT * FROM comments where submission_id=\'" + submission_row['id'] + "\'",
             con=engine)
-        for _,comment_row in comments.iterrows():
+        for _, comment_row in comments.iterrows():
+            if type(comment_row['body']) is NoneType:
+                continue
             comment_length = len(comment_row['body'])
             df2.loc[len(df2)] = [comment_length, comment_row["score"]]
 
@@ -36,9 +52,9 @@ def subreddit_length(name):
 
 subreddits = pd.read_sql_table("subreddits", con=engine)
 selection = input("Subreddit name:")
-if selection == "all":
-    pool = ThreadPool(4) 
+if selection == "":
+    pool = ThreadPool(4)
     pool.map(subreddit_length, subreddits['id'])
 else:
-    results = subreddits.where(subreddits['display_name']==selection).dropna()
+    results = subreddits.where(subreddits['display_name'] == selection).dropna()
     subreddit_length(results.iloc[0]['id'])
--- data-stats/most-common-words.py
+++ data-stats/most-common-words.py
@@ -1,14 +1,18 @@
 import pandas as pd
-import pandas.io as io
 from sqlalchemy import create_engine
-import threading
-from multiprocessing.dummy import Pool as ThreadPool 
+from multiprocessing.dummy import Pool as ThreadPool
 import re
-engine = create_engine('postgresql://postgres:password123@localhost:5432/reddit')
+
+from sqlalchemy.util import NoneType
+
+import os.path
+
+engine = create_engine('postgresql://postgres@localhost:5432/reddit')
 
 
 def subreddit_resolve(name):
-    regex = re.compile('[,\.!?\'\"]')
+    if os.path.isfile("submission_count_{0}.csv".format(name)):
+        return
     submissions = pd.read_sql_query(
         "SELECT * FROM submissions where subreddit_id=\'{0}\'".format(name),
         con=engine)
@@ -18,25 +22,33 @@ def subreddit_resolve(name):
     df2 = pd.DataFrame(columns=["word", "uses", "unique", "score"])
     df2.set_index('word', drop=True, inplace=True)
 
-    for submission_index,submission_row in submissions.iterrows():
-        print("Submission: {0}/{1}".format(submission_index+1, submissions['id'].count()))
-        words = regex.sub('',submission_row['title'].lower()).split(" ")
-        words.extend(submission_row['selftext'].lower().split(" "))
-        for word in words:
+    count = submissions['id'].count()
+    for submission_index, submission_row in submissions.iterrows():
+        print("Submission: {0}/{1}".format(submission_index + 1, count))
+        if type(submission_row['title']) is not NoneType:
+            words = submission_row['title'].lower().split(" ")
+        else:
+            words = []
+        if type(submission_row['selftext']) is not NoneType:
+            words.extend(submission_row['selftext'].lower().split(" "))
+
+        for word in set(words):
             if word not in df.index:
                 df.loc[word] = [0] * df.columns.size
-            df['uses'][word] += 1
-        for word in set(words):
             df["unique"][word] += 1
             df["score"][word] += submission_row['score']
+        for word in words:
+            df['uses'][word] += 1
 
         # Comments
         comments = pd.read_sql_query(
-            "SELECT * FROM comments where parent_id=\'"+submission_row['id']+"\'",
+            "SELECT * FROM comments where submission_id=\'" + submission_row['id'] + "\'",
             con=engine)
 
-        for _,comment_row in comments.iterrows():
-            comment_words = regex.sub('',comment_row['body'].lower()).split(" ")
+        for _, comment_row in comments.iterrows():
+            if type(comment_row['body']) is NoneType:
+                continue
+            comment_words = comment_row['body'].lower().split(" ")
             for word in comment_words:
                 if word not in df2.index:
                     df2.loc[word] = [0] * df2.columns.size
@@ -47,14 +59,16 @@ def subreddit_resolve(name):
 
     df.sort_values(["score"], ascending=False, inplace=True)
     df2.sort_values(["score"], ascending=False, inplace=True)
-    df.to_csv(path_or_buf="{0}/submission_count_{0}.csv".format(name))
-    df2.to_csv(path_or_buf="{0}/comment_count_{0}.csv".format(name))
+    df.to_csv(path_or_buf="submission_count_{0}.csv".format(name))
+    df2.to_csv(path_or_buf="comment_count_{0}.csv".format(name))
+
 
 subreddits = pd.read_sql_table("subreddits", con=engine)
+subreddits = subreddits.sample(frac=1)
 selection = input("Subreddit name:")
-if selection == "all":
-    pool = ThreadPool(4) 
+if selection == "":
+    pool = ThreadPool(4)
     pool.map(subreddit_resolve, subreddits['id'])
 else:
-    results = subreddits.where(subreddits['display_name']==selection).dropna()
+    results = subreddits.where(subreddits['display_name'] == selection).dropna()
     subreddit_resolve(results.iloc[0]['id'])
