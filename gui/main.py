3465fd1b80cf4f47a426d4e45f894f347d9e768e
Igor S <igors@anche.no>
2019-05-25 22:42:29 +0200

    set the mockup in motion

---
+++ gui/main.py
@@ -0,0 +1,19 @@
+import sys
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from gui_main_window import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.setWindowTitle("REDDIT CALCULATOR xD")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
