b26d6a3ca53b4b79257bafbd29a60a5e3265ad72
Igor S <igors@anche.no>
2019-05-27 16:16:42 +0200

    karma_length: fix crashing on multiplies of 50

--- gui/main.py
+++ gui/main.py
@@ -11,11 +11,10 @@ import numpy as np
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
 from gui_main_window import Ui_MainWindow
 
-# TODO: - UnicodeDecodeError: 'charmap' codec can't decode byte X encoding="utf8")
-# TODO:  Karma based on Length crashuje na i % 50 = 0
-# TODO:  Mean sentiments ma za wysoki próg "very likely" do positive i negative sentimentu
-# TODO:  Mean sentiment ma odwrócone wartości, minusy to pozytywne, plusy to negatywne
-# TODO:  Mean sentiments zwraca zerowe raw data
+# TODO: UnicodeDecodeError: 'charmap' codec can't decode byte X encoding="utf8")
+# TODO: Mean sentiments ma za wysoki próg "very likely" do positive i negative sentimentu
+# TODO: Mean sentiment ma odwrócone wartości, minusy to pozytywne, plusy to negatywne
+# TODO: Mean sentiments zwraca zerowe raw data
 
 class DaysOfWeek(Enum):
     MONDAY = 0
@@ -248,12 +247,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 result = next(itertools.islice(reader, length, None))[self.subreddits.index(sub) + 1]
             else:
                 total = 0
+                i = 0
                 lbound = math.floor(length / 50) * 50 + 1
-                ubound = math.ceil(length / 50) * 50
-                print(lbound, ubound)
+                ubound = math.ceil((length + 1) / 50) * 50
+                print("calculating avg in the interval:", lbound, ubound)
                 for row in list(reader)[lbound:ubound]:
                     total += float(row[self.subreddits.index(sub) + 1])
-                result = total / 50
+                    i += 1
+                result = total / i
 
             print("Result:", result)
             if self.lengthRawDataCheckBox.isChecked():
